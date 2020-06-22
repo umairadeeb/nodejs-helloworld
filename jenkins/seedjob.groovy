fda8581c734c2fa71238dd7f2442b18320cbccf6
umair.adeeb <umair.adeeb@wirecard.com>
2020-06-22 18:15:18 +0200

    Adding seed job.

---
+++ jenkins/seedjob.groovy
@@ -0,0 +1,29 @@
+// CI Job for NodeJS App
+multibranchPipelineJob("ci-build-job") {
+    displayName 'CI Build'
+
+    orphanedItemStrategy {
+        discardOldItems {
+            numToKeep(10)
+        }
+    }
+
+    branchSources {
+        github {
+           // scanCredentialsId('github-ci-user-credentials')
+            repoOwner('umairadeeb')
+            repository('nodejs-helloworld')
+            buildOriginBranch(true)
+            buildOriginPRMerge(false)
+            buildOriginBranchWithPR(false)
+            id("nodejs-helloworld-branch-id")
+        }
+    }
+
+    factory {
+        workflowBranchProjectFactory {
+            scriptPath('Jenkinsfile')
+        }
+    }
+}
+}
